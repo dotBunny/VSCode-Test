83a8e3826061fb99825ba5199fc6d5d0c5729fa9
Matthew Davey <matthew.davey@dotbunny.com>
2015-09-21 14:45:12 -0400

    Initial Import

---
+++ Assets/Tester.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Tester : MonoBehaviour {
+    public static float TimeTest = 0f;
+    float Timestamp = 0;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        Timestamp = Time.time;
+        TimeTest = Timestamp;
+        if ( Timestamp < 0 ) {
+            Debug.Log("BLAH");
+        }
+    }
+    
+    public void OnGUI()
+    {
+        if (GUILayout.Button("Trigger") ) {
+            Debug.Log("TRIGGERED");
+        }
+    }
+}
